7cd41177fbb3601f56db0ea70cd2a91a5c5663c2
Karan <karanchandekar431@gmail.com>
2022-11-16 15:43:00 +0530

    added more text in why authx section

--- src/Components/Section2/Section2.jsx
+++ src/Components/Section2/Section2.jsx
@@ -10,14 +10,19 @@ const Section2 = () => {
           Why <span>AuthX</span> ?
         </h1>
         <p>
-          AuthX is very important because UX is the biggest barrier in user
-          growth across Web3 applications. Masses are not comfortable with
-          wallets, Seed phrase, private key and all such stuff. They need easier
-          ways to interact with web3, ideally what they have been using in web2.
+          Exchange are Money Bridge, Oracles are Data Bridge, similarly AUTHX is
+          Identity Bridge between Web2 and Web3.
+        </p>
+        <p>
+          UX is the biggest barrier in user growth across Web3 applications.
+          Masses are not comfortable with wallets, Seed phrase, private key and
+          all such stuff. They need easier ways to interact with web3, ideally
+          what they have been using in web2.
         </p>
         <p>
           AuthX enables web3 apps to leverage the simplicity and distribution of
-          web2 user id's to onboard new users and offer them a smooth UX.
+          web2 user id's to onboard new users and offer them a smooth UX in
+          onboarding and navigating the Web3.
         </p>
       </Element>
     </>
